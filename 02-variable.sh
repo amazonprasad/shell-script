44e36ccc117ac457f4a60a233065c6e774e5582d
muni <muni@gmail.com>
2024-07-01 18:24:48 +0530

    01-date.sh

---
+++ 02-variable.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+person1=prasad
+person2=muni
+
+echo "$person1: hai Good Morning"
+echo "$person2: good morning, Whatsapp"
+echo "$person1: Nothing Thank you"
+echo "$person2: Thank you for asking"
+
