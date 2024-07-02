e805224cb970fd6bd84262883d2d2af5b666a4b2
muni <muni@gmail.com>
2024-07-02 16:01:56 +0530

    01-date.sh

---
+++ program.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+Date=$(date +%F)
+Script_name=$0
+Logfile=/tmp/$Script_name-$Date.log
+
+Validate(){
+    if [ $1 -ne 0 ]
+    then 
+        echo -e "$2...$R Failure$N"
+    else 
+        echo -e "$2...$G Success$N"
+    fi 
+
+}
+ userID=$(id -u)
+ if [ $userID -ne 0 ]
+ then 
+    echo "Error: Please ruin this script as a root user"
+else
+    echo " You are the root user"
+fi 
+
+yum install java -y >> $Logfile
+Validate $? "installing Java"
