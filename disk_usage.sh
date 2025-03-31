52a14e839f12fa04500dc4a1046fc1a7733e7fa1
muni <muni@gmail.com>
2025-03-31 23:04:10 +0530

    hello

--- disk_usage.sh
+++ disk_usage.sh
@@ -1,3 +1,19 @@
 #!bin/bash
 
 DISK_USAGE=$(df -hT | grep -vE 'tmp|File')
+DISK_THRESHOLD=1
+message=""
+ 
+while IFS= read line
+do 
+
+usage=$(echo $line | awk '{print $6F}' | cut -d % -f1)
+partition=$(echo #line | qwk '{print $1F}')
+if [ $usage -ge $DISK_THRESHOLD ]
+then 
+    message+="High Disk usage on $partition:$usage /n"
+fi 
+done <<< $DiSK_USAGE 
+
+
+ 
