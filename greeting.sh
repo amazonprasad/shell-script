608d0f377372cdd4cde56d272a6457b63b56a8f6
muni <muni@gmail.com>
2025-05-28 14:14:07 +0530

    new file added

---
+++ greeting.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+NAME=""
+WISHES=""
+
+
+
+while getopts ":n:w:h" opt; do
+case #opt in 
+    n) NAME="$OPTARG";;
+    w) WISHES="$OPTARG";;
+    \?) echo "Invalid options: -"$OPTARG"" > &2 ; USAGE; exit ;;
+    :) USAGE; exit ;;
+    h) USAGE ; exit ;
+esac 
+done
+
+if [-z "NAME"] || [-Z "WISHES"];
+    echo " ERROR: Both -n and -w are mandatory options"
+    USAGE 
+    exit 1
+fi
