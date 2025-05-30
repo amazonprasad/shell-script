95647d7c3e18426bd962e98d82e8345b6cff195e
muni <muni@gmail.com>
2025-05-30 17:36:16 +0530

    create a file

--- create_Instance.sh
+++ create_Instance.sh
@@ -2,7 +2,7 @@
 
 AMI=ami-0b4f379183e5706b9
 SECURITY_ID=sg-02a430c742a1d56b7
-INSTANCE=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" 
+INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" 
 "dispatch" "web")
 ZONE_ID=Z03833742M0KF7ERPTLO0
 DOMAIN_NAME=manacars.shop
@@ -13,7 +13,7 @@ do
     then
         INSTANCE_TYPE="t3.small"
     else
-        INSTANCE_TYPE="t2.smal"
+        INSTANCE_TYPE="t2.micro"
     fi
     IP_Adress=$(aws ec2 run-instances --image-id ami-0b4f379183e5706b9 --instance-type $INSTANCE_TYPE
     --security-group-ids sg-02a430c742a1d56b7 --tag-specifications "ResourseType=instance,
