68a99a3ab2753b3ea780076e4796c48fcf7f58b1
muni <muni@gmail.com>
2025-05-30 17:21:19 +0530

    create a file

---
+++ create_Instance.sh
@@ -0,0 +1,44 @@
+#/bin/bash
+
+AMI=ami-0b4f379183e5706b9
+SECURITY_ID=sg-02a430c742a1d56b7
+INSTANCE=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" 
+"dispatch" "web")
+ZONE_ID=Z03833742M0KF7ERPTLO0
+DOMAIN_NAME=manacars.shop
+
+for i in "${Instances[@]}"
+do 
+    if [ $i == "mongodb" ] || [ $i == "mysql" ] || [ $i == "shipping" ]
+    then
+        INSTANCE_TYPE="t3.small"
+    else
+        INSTANCE_TYPE="t2.smal"
+    fi
+    IP_Adress=$(aws ec2 run-instances --image-id ami-0b4f379183e5706b9 --instance-type $INSTANCE_TYPE
+    --security-group-ids sg-02a430c742a1d56b7 --tag-specifications "ResourseType=instance,
+    Tags=[{Key=NAME,Value=$i}]" --query 'Instance[0]. PrivateIpAddres' --output text) 
+done
+echo "$i:$IP_Adress"
+
+aws route53 change-resource-record-sets \
+--hosted-zone-id  $ZONE_ID\
+--change-batch "
+{
+    "comment": Testing creating a record set ",
+    "changes": [{
+    "Action"                : "CREATE",
+    ,"ResourceRecordSet"    : {
+     "Name"                 : "$i.$DOMAIN_NAME"
+     ,"Type"                : "A"
+     ,"TTL"                 : 1,
+     ,"ResourceRecords"     : [{
+        "Value"             : "$IP_Adress"
+     }]
+    }
+    }]
+}
+"
+done 
+
+
