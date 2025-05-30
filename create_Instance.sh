35c0c2ee6a6a882f530aa8fa4217e915e5bea522
muni <muni@gmail.com>
2025-05-30 19:25:37 +0530

    create a file

--- create_Instance.sh
+++ create_Instance.sh
@@ -14,8 +14,9 @@ do
         INSTANCE_TYPE="t2.micro"
     fi
     aws ec2 run-instances --image-id ami-0b4f379183e5706b9 --instance-type $INSTANCE_TYPE 
-    --security-group-ids $SECURITY_ID  --tag-specifications 
-    "ResourseType=instance,Tags=[{Key=NAME,Value=$i}]" 
+    --security-group-ids $SECURITY_ID --tag-specifications
+     "ResourceType=instance,Tags=[{Key=NAME,Value=$i}]"
+    
 
 done
 
