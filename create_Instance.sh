04e865e2744d2e63ded929315b393d9e52ff6417
muni <muni@gmail.com>
2025-05-30 17:49:54 +0530

    create a file

--- create_Instance.sh
+++ create_Instance.sh
@@ -13,8 +13,7 @@ do
     else
         INSTANCE_TYPE="t2.micro"
     fi
-    IP_Adress=$(aws ec2 run-instances --image-id ami-0b4f379183e5706b9 --instance-type $INSTANCE_TYPE
-    --security-group-ids $SECURITY_ID  --tag-specifications "ResourseType=instance,
+    IP_Adress=$(aws ec2 run-instances --image-id ami-0b4f379183e5706b9 --instance-type $INSTANCE_TYPE --security-group-ids $SECURITY_ID  --tag-specifications "ResourseType=instance,
     Tags=[{Key=NAME,Value=$i}]" --query 'Instance[0]. PrivateIpAddres' --output text) 
 done
 
