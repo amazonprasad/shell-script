bd6454e2dca6e3005cf9949fe08241da1471d656
muni <muni@gmail.com>
2025-06-01 00:57:04 +0530

    create a file

--- create_Instance.sh
+++ create_Instance.sh
@@ -13,4 +13,7 @@ do
     else
         INSTANCE_TYPE="t2.micro"
     fi
+
+    aws ec2 run-instances --image-id ami-0b4f379183e5706b9 --instnce-type $INSTANCE_TYPE
+    --security-group-ids sg-02a430c742a1d56b7
 done 
