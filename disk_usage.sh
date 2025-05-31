9566624d1bafacda36a19dbfcc28242f6f231001
muni <muni@gmail.com>
2025-06-01 03:55:22 +0530

    create a file

--- disk_usage.sh
+++ disk_usage.sh
@@ -18,4 +18,4 @@ echo -e "Message: $message"
 
 #echo "$message" | mail -s "High Disk Usage" muniprasad845@gmail.com
 
-sh mail.sh "DevOps Team" "High Disk Usage" "$message" "muniprasad605@gmail.com" "ALERT High Disk Usage"
+sh mail.sh "DevOps Team" "High Disk Usage" "$message" "muniprasad845@gmail.com" "ALERT High Disk Usage"
