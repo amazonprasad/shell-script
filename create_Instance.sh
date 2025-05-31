d31e5c960c6b44e75362496761132c1ca319425e
muni <muni@gmail.com>
2025-06-01 00:51:01 +0530

    create a file

--- create_Instance.sh
+++ create_Instance.sh
@@ -2,23 +2,15 @@
 
 AMI=ami-0b4f379183e5706b9
 SECURITY_ID=sg-02a430c742a1d56b7
-INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" 
-"dispatch" "web")
+INSTANCES=("mongodb" "catalogue" "cart" "user" "redis" "mysql" "shipping" "rabbitmq" 
+"payment" "dispatch" "web")   
 
 for i in "${INSTANCES[@]}"
-do 
-    echo "instance is: $i"
+do
     if [ $i == "mongodb" ] || [ $i == "mysql" ] || [ $i == "shipping" ]
-    then
-        INSTANCE_TYPE="t3.small"
+    then 
+        INSTANCE_TYPE="t3.smalll"
     else
         INSTANCE_TYPE="t2.micro"
     fi
-    aws ec2 run-instances --image-id ami-0b4f379183e5706b9 --instance-type $INSTANCE_TYPE 
-    --security-group-ids $SECURITY_ID --tag-specifications
-     "ResourceType=instance,Tags=[{Key=NAME,Value=$i}]" 
-    
-
-done
-
-
+done 
